b7e531d45c138b901bf7d11a73362dd0a20dde88
riesenfeld <aron.riesenfeld@gmail.com>
2022-08-26 22:16:22 -0700

    made footer more legible

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -4,7 +4,6 @@
       class="footer-link email"
       href="mailto:localspacepatrolbooking@gmail.com"
     >
-      <!-- loca<span>l</span>spac<span>e</span>patro<span>l</span>bookin<span>g</span>@gmail.com -->
       <span>local</span><span>space</span><span>patrol</span><span>booking</span
       ><span><span>@</span>gmail.com</span>
     </a>
@@ -33,24 +32,25 @@ footer {
 }
 
 .footer-link {
-  /* color: rgba(150, 20, 150, 1); */
   color: rgb(228, 100, 228);
-  /* color: rgba(150, 220, 150, 1); */
-  /* color: rgba(130, 190, 130, 1); */
-  /* color: rgb(115, 167, 115); */
   text-shadow: 2px 2px 3px rgba(20, 20, 80, 1),
     -2px -2px 3px rgba(20, 20, 80, 1), -2px 2px 3px rgba(20, 20, 80, 1),
     2px -2px 3px rgba(20, 20, 80, 1);
 }
 
 .footer-link span {
-  /* letter-spacing: 2px; */
   margin-right: 1px;
 }
 
 @media (orientation: portrait) and (max-width: 600px) {
   footer {
     font-size: 3vw;
+  }
+}
+
+@media (min-aspect-ratio: 16/9) {
+  footer {
+    font-size: 2vw;
   }
 }
 </style>
