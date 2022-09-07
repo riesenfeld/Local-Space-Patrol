cc88304d713ff0eb78d294136527816687bd475e
riesenfeld <aron.riesenfeld@gmail.com>
2022-09-06 19:34:52 -0700

    a little cleanup

--- pages/TestingPage.vue
+++ pages/TestingPage.vue
@@ -4,8 +4,6 @@
     <div v-for="track in tracks" :key="track.id">
       <h2>{{ track.title }}</h2>
       <h3>Published at: {{ makeDateTime(track.published_at) }}</h3>
-      <h5>{{ makeDate(track.published_at) }}</h5>
-      <h5>{{ makeTime(track.published_at) }}</h5>
       <h4>object id: {{ track.id }}</h4>
       <h4>media id: {{ track.metadata.media_data.id }}</h4>
       <audio controls :src="track.metadata.audio.url"></audio>
@@ -34,16 +32,6 @@ export default {
       const d = new Date(str)
       const dateString = d.toLocaleString('en-US')
       return dateString
-    },
-    makeDate(str) {
-      const d = new Date(str)
-      const dateString = d.toLocaleDateString('en-US')
-      return dateString
-    },
-    makeTime(str) {
-      const d = new Date(str)
-      const timeString = d.toLocaleTimeString('en-US')
-      return timeString
     },
   },
 }
