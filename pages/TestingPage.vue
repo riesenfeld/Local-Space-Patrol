e93665b511856687f618689d0ec859c8b01b25c6
riesenfeld <aron.riesenfeld@gmail.com>
2022-09-06 19:34:17 -0700

    fetch-tracks in working order

--- pages/TestingPage.vue
+++ pages/TestingPage.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
-    <h2>Tracks</h2>
-    {{ tracks }}
-    <!-- {{ tracks['tracks'].objects[0] }} -->
-    <!-- <div v-for="track in tracks" :key="track.id">
-      <h3>{{ track }}</h3>
+    <h1>Tracks</h1>
+    <div v-for="track in tracks" :key="track.id">
+      <h2>{{ track.title }}</h2>
+      <h3>Published at: {{ makeDateTime(track.published_at) }}</h3>
+      <h5>{{ makeDate(track.published_at) }}</h5>
+      <h5>{{ makeTime(track.published_at) }}</h5>
       <h4>object id: {{ track.id }}</h4>
       <h4>media id: {{ track.metadata.media_data.id }}</h4>
       <audio controls :src="track.metadata.audio.url"></audio>
-    </div> -->
+    </div>
   </div>
 </template>
 
@@ -18,17 +19,32 @@ export default {
     const response = await fetch(
       'https://fantastic-biscuit-65517c.netlify.app/.netlify/functions/fetch-tracks'
     )
-    const tracks = await response.json()
-    // const tracks = await response.text()
-    console.log(tracks)
+    const json = await response.json()
     return {
-      tracks,
+      tracks: json.objects,
     }
   },
   data() {
     return {
       tracks: null,
     }
+  },
+  methods: {
+    makeDateTime(str) {
+      const d = new Date(str)
+      const dateString = d.toLocaleString('en-US')
+      return dateString
+    },
+    makeDate(str) {
+      const d = new Date(str)
+      const dateString = d.toLocaleDateString('en-US')
+      return dateString
+    },
+    makeTime(str) {
+      const d = new Date(str)
+      const timeString = d.toLocaleTimeString('en-US')
+      return timeString
+    },
   },
 }
 </script>
