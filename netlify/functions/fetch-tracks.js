c6a94580067e53f58c6bde181ae56a0e74a29ab3
riesenfeld <aron.riesenfeld@gmail.com>
2022-09-06 17:43:32 -0700

    using Netlify env vars, put placeholder code in delete-track and upload-track

--- netlify/functions/fetch-tracks.js
+++ netlify/functions/fetch-tracks.js
@@ -1,14 +1,11 @@
 import nodeFetch from 'node-fetch'
 
-/* MOVE TO ENV VARS */
-const BUCKET_SLUG = 'template-vue-music-website-production'
-const READ_KEY = 'GqdIpxdAn1W44s8FMYdSyJ6edqijwLtx2b5BkF6qHztVjza7jp'
-const OBJECT_TYPE = 'audio'
-
 const getObjects = async function () {
   const response = await nodeFetch(
-    `https://api.cosmicjs.com/v2/buckets/${BUCKET_SLUG}/objects?read_key=${READ_KEY}&query=${encodeURIComponent(
-      `{ "type": "${OBJECT_TYPE}" }`
+    `https://api.cosmicjs.com/v2/buckets/${
+      process.env.BUCKET_SLUG
+    }/objects?read_key=${process.env.READ_KEY}&query=${encodeURIComponent(
+      `{ "type": "${process.env.OBJECT_TYPE}" }`
     )}`
   )
   return response.json()
---
+++ netlify/functions/upload-track.js
@@ -0,0 +1,27 @@
+/**
+ * TEMPORARY PLACEHOLDER CODE
+ */
+
+import nodeFetch from 'node-fetch'
+
+const getObjects = async function () {
+  const response = await nodeFetch(
+    `https://api.cosmicjs.com/v2/buckets/${
+      process.env.BUCKET_SLUG
+    }/objects?read_key=${process.env.READ_KEY}&query=${encodeURIComponent(
+      `{ "type": "${process.env.OBJECT_TYPE}" }`
+    )}`
+  )
+  return response.json()
+}
+
+exports.handler = async function () {
+  const response = {
+    tracks: await getObjects(),
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(response),
+  }
+}
